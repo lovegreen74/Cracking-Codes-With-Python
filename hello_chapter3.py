e8c319cff8660e5b4d82d6153714413c69e43b4e
Nq_nnhu <75362571+lovegreen74@users.noreply.github.com>
2020-12-07 14:19:48 +0100

    Add files via upload

---
+++ hello_chapter3.py
@@ -0,0 +1,11 @@
+# This program says hello and asks for my name.There are two ways to compute this program.
+#%%
+print('Hello,world!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+#%%
+def myName():
+    print("Hi guys")
+    myName = input("What is your name? ")
+    print ("It is good to meet you,", myName)
